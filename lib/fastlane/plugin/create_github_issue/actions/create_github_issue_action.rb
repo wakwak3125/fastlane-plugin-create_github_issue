861a2dd8a37fd0eed04fcf09cf1fcbc2e17121ff
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2018-12-10 14:30:13 +0900

    Implement

--- lib/fastlane/plugin/create_github_issue/actions/create_github_issue_action.rb
+++ lib/fastlane/plugin/create_github_issue/actions/create_github_issue_action.rb
@@ -5,7 +5,19 @@ module Fastlane
   module Actions
     class CreateGithubIssueAction < Action
       def self.run(params)
-        UI.message("The create_github_issue plugin is working!")
+        other_action.github_api(
+          server_url: "https://api.github.com",
+          api_token: params[:api_token],
+          http_method: 'POST',
+          path: "/repos/#{params[:repo]}/issues",
+          body: {
+            title: params[:title],
+            body: params[:body] || "",
+            labels: params[:labels] || [],
+            assignees: params[:assignees] || [],
+            milestone: params[:milestone]
+          }
+        )
       end
 
       def self.description
@@ -27,21 +39,48 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "CREATE_GITHUB_ISSUE_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :api_token,
+                                  env_name: "CREATE_GITHUB_ISSUE_GIT_HUB_TOKEN",
+                               description: "Your github token",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :repo,
+                                  env_name: "CREATE_GITHUB_ISSUE_REPOSITORY",
+                               description: "Repository that you want to create an issue, format -> owner/name",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :title,
+                                  env_name: "CREATE_GITHUB_ISSUE_TITLE",
+                               description: "Issue title",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :body,
+                                  env_name: "CREATE_GITHUB_ISSUE_BODY",
+                               description: "Issue body",
+                                  optional: true,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :labels,
+                                       env_name: "CREATE_GITHUB_ISSUE_LABELS",
+                                       description: "Issue labels",
+                                       optional: true,
+                                       type: Array),
+          FastlaneCore::ConfigItem.new(key: :assignees,
+                                       env_name: "CREATE_GITHUB_ISSUE_ASSIGNEES",
+                                       description: "Issue assignees",
+                                       optional: true,
+                                       type: Array),
+          FastlaneCore::ConfigItem.new(key: :milestone,
+                                       env_name: "CREATE_GITHUB_ISSUE_MILESTONE",
+                                       description: "Issue milestone",
+                                       optional: true,
+                                       type: String)
         ]
       end
 
       def self.is_supported?(platform)
-        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
-        # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
-        #
-        # [:ios, :mac, :android].include?(platform)
         true
       end
     end
   end
 end
+
